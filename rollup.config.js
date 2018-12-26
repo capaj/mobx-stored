01cd97fdba946f7ff7550e2cc50d48037cd0ec43
Jiri Spac <capajj@gmail.com>
2018-12-26 15:35:33 +0100

    adds external config to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import typescript from 'rollup-plugin-typescript2'
 export default {
   input: 'src/stored-observable.ts',
   plugins: [typescript()],
+  external: (id) => !id.startsWith('.') && !id.startsWith('/'),
   output: [
     { file: 'dist/stored-observable.cjs.js', format: 'cjs' },
     { file: 'dist/stored-observable.es.js', format: 'es' }
